ffb0e7a04f3a990c6cb9c01a1105c513838a1fc8
Abhishek Das <dasabhishek164@gmail.com>
2023-11-01 17:47:33 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", { id: "heading" }, "Hello Abhishek from React 3.0");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
